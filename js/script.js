78a51b34c4d16a60e89c552759d95dbf63854bd8
“Emily <Emilyparrasalazar2@gmail.com>
2022-07-25 22:43:53 -0300

    agrego js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let opcion= prompt('Agregar la opcion de su preferencia: \n1 - Magdalenas blueberry \n2 - Torta sweet dream \n3 - Torta te gree \n4 - Torta pinky \n5 - Torta Corazon \n6 - Magdalenas mouse \n7 - Magdalenas fruto rojo \n8 - Magdalenas florales');
+
+switch (opcion) {
+    case '1':
+        alert('Elegiste Magdalenas blueberry');
+        break;
+    case '2':
+        alert('Elegiste Torta sweet dream');
+        break;
+    case '3':
+        alert('Elegiste Torta te gree');
+        break;
+    case '4':
+        alert('Elegiste Torta pinky');
+        break;
+    case'5':
+        alert('Elegiste Torta corazon'); 
+        break;
+    case '6':
+        alert('Elegiste Magdalenas mouse');
+        break;
+    case '7':
+        alert('Elegiste Magdalenas fruto rojo');
+        break;
+    case '8':
+        alert('Elegiste Magdalenas florales');
+        break; 
+    default:
+        alert('El numero que ingresaste es incorrecto')
+        break;
+}
+
