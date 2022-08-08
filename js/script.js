74a250a4ba13bdcd28561620acb134f5a7b7505e
“Emily <Emilyparrasalazar2@gmail.com>
2022-08-08 12:32:01 -0300

    array

--- js/script.js
+++ js/script.js
@@ -1,107 +1,192 @@
+class Productos {
+    constructor(nombre, precio, categoria) {
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
+        this.categoria = categoria.toUpperCase();
+        this.cantidad = 0;
+        this.stock = true;
+    }
+    sinStock() {
+        this.stock = false;
+    }
+}
+//Crea los productos
+function getProductos(listaProductos) {
 
-function inicio() {
-let compra = true;
+        let tortaCorazon = new Productos("Torta de corazon", 3600, "Torta");
+        let magdalenaTutifruti = new Productos("Magdalenas tutifruti", 250, "Magdalena");
+        let tortaPinky = new Productos("Torta pinky", 2000, "Torta") ;
+        let magdalenasBlueberry = new Productos("Magdalena blueberry", 300 , "Magdalena"); 
+        let tortaSweetDream = new Productos("Torta sweet dream", 6950, "Torta");
+        let magdalenasMouse = new Productos("Magdalena mouse", 360, "Magdalena");
+        let tortaTeGree = new Productos("Torta te gree", 6900, "Torta");
+        let magdalenasFlorales = new Productos("Magdalena florales", 300, "Magdalena");
+        listaProductos[0] = tortaCorazon;
+        listaProductos[1] = magdalenaTutifruti;
+        listaProductos[2] = tortaPinky;
+        listaProductos[3] = magdalenasBlueberry;
+        listaProductos[4] = tortaSweetDream;
+        listaProductos[5] = magdalenasMouse;
+        listaProductos[6] = tortaTeGree;
+        listaProductos[7] = magdalenasFlorales;
+    }
+//pushea el producto al carrito
+const carrito = function (miCarrito, producto) { miCarrito.push(producto); }
+//Suma el precio 
+const suma = (n1, n2) => n1 + n2;
+//Calcula el precio final
+function getPrecioFinal(miCarrito) {
+    let precioFinal = 0;
+    for (let i = 0; i < miCarrito.length; i++) {
+        precioFinal = precioFinal + (miCarrito[i].cantidad * miCarrito[i].precio)
+    }
+    return precioFinal;
+}
+
+//Muestra la cuenta en un alert
+function showCarrito(miCarrito) {
+    let string = "Su pedido es:\n";
+    for (let i = 0; i < miCarrito.length; i++) {
+        string = string + miCarrito[i].cantidad + " " + miCarrito[i].nombre + ".......$" + (miCarrito[i].precio * miCarrito[i].cantidad) + "\n";
+    }
+    string = string + "Su total a pagar es de : $" + getPrecioFinal(miCarrito);
+    return string;
+}
+//Agrega una cantidad al producto y si es 0 llama a la funcion para ponerlo en el carrito
+function agregar(producto, miCarrito) {
+    if (producto.cantidad == 0) {
+        producto.cantidad++;
+        carrito(miCarrito, producto);
+    } else {
+        producto.cantidad++;
+    }
+}
+//Restar la cantidad de productos y si es igual a 0 sacar al producto
+function sacar(producto, miCarrito) {
+    if (producto.cantidad == 1) {
+        producto.cantidad--;
+        miCarrito.splice(miCarrito.indexOf(producto), 1);
+    } else if (producto.cantidad > 1) {
+        producto.cantidad--;
+    } else { }
+}
+//Ingresar los distintos productos
+function llenarCarrito(listaProductos, miCarrito) {
+    let compra = true;
     while (compra) {
-        let opcion = prompt("Ingrese  el numeron a la opcion a realizar \n1-Ir al producto \n2 -Agregar al carrito \n3-Cancelar y salir");
+        let opcion = prompt("Del 1 al 8 se usan para seleccionar los productos y 9 finalizar el pedido\nEl menu es:\n" + listaProductos[0].nombre + ":" + listaProductos[0].precio + "\n" + listaProductos[1].nombre + ":" + listaProductos[1].precio + "\n" + listaProductos[2].nombre + ":" + listaProductos[2].precio + "\n"+  listaProductos[3].nombre + ":" + listaProductos[3].precio +"\n" + listaProductos[4].nombre + ":" + listaProductos[4].precio + "\n"+ listaProductos[5].nombre + ":" + listaProductos[5].precio + "\n"+ listaProductos[6].nombre + ":" + listaProductos[6].precio + "\n"+ listaProductos[7].nombre + ":" + listaProductos[7].precio + "\n"+ showCarrito(miCarrito));
         switch (opcion) {
             case "1":
-                irAlProducto();
+                agregar(listaProductos[0], miCarrito)
                 break;
             case "2":
-                agregaralCarrito();
-                compra = false;
+                agregar(listaProductos[1], miCarrito)
+                break;
             case "3":
+                agregar(listaProductos[2], miCarrito)
+                break;
+            case "4":
+                agregar(listaProductos[3], miCarrito)
+                break;
+            case "5":
+                agregar(listaProductos[4], miCarrito)
+                break;
+            case "6":
+                agregar(listaProductos[5], miCarrito)
+                break;
+            case "7":
+                agregar(listaProductos[6], miCarrito)
+                break;
+            case "8":
+                agregar(listaProductos[7], miCarrito)
+                break;
+            case "9":
                 compra = false;
                 break;
-
             default:
-                alert('La opcion que ingresaste es incorrecto');
+                alert("La opcion ingresada fue invalida intentelo de nuevo.");
         }
     }
- }
-    const suma = (n1, n2) => n1 + n2;
-    const irAlProducto = function () { alert('Productos:\n1 -Torta corazon: 3600$ \n2 -Magdalenas frutos rojos:250$ c/u \n3 -Torta pinky:2000$ \n4 -Magdalenas blueberry:300$ c/u  \n5 -Torta sweet dream:6950$  \n6 -Magdalenas mouse:360$ c/u \n7 -Torta te gree:6900$ \n8 -Magdalenas florales:300$ c/u'); } 
-    const agregarProductos = (lista, productos) => lista + "\n" + productos;
-    const finalizarCompra = function (total, lista) { alert("El total es de:" + lista + "\nEl total es de: " + total+"$") }
-
-    function agregaralCarrito() {
-        let elTotal = 0 ;
-        let listaProductos = "" ;
-        const precioTortaCorazon = 3600;
-        const precionMagdalenasFrutosRojos = 250 ;
-        const precioTortaPinky = 2000;
-        const precioMagdalenasBlueberry = 300 ;
-        const precioTortaSweetDream = 6950;
-        const precioMagdalenasMouse = 360;
-        const precioTortaTeGree = 6900;
-        const precioMagdalenasFlorales = 300;
-        
-        let compra = true;
-        while (compra) {
-        let opcion = prompt("Que opcion desea agregar. Ingrese 9 para ver los productos y la opcion 10 para terminar");
+}
+//Ingresar la opcion de los productos a borrar
+function vaciarCarrito(miCarrito) {
+    let compra = true;
+    while (compra) {
+        let opcion = prompt("Del 1 al 8 se usan para seleccionar los productos y 9 Finalizar compra \n" + showCarrito(miCarrito));
         switch (opcion) {
-                case "1":
-                    elTotal = suma(elTotal, precioTortaCorazon);
-                    listaProductos = agregarProductos(listaProductos, '-Torta corazon: 3600$');
-                    console.log(elTotal);
-                    alert(listaProductos)
-                    break;
-                case "2":
-                    elTotal = suma(elTotal, precionMagdalenasFrutosRojos);
-                    listaProductos = agregarProductos(listaProductos, '-Magdalenas frutos rojos:250$ c/u');
-                    console.log(elTotal);
-                    alert(listaProductos)
-                    break;
-                case "3":
-                    elTotal = suma(elTotal, precioTortaPinky);
-                    listaProductos = agregarProductos(listaProductos, '-Torta pinky:2000$');
-                    console.log(elTotal);
-                    alert(listaProductos)
-                    break;
+            case "1":
+                if (miCarrito[0] != undefined) {
+                    sacar(miCarrito[0], miCarrito);
+                }
 
-                case "4":
-                    elTotal = suma(elTotal,  precioMagdalenasBlueberry);
-                    listaProductos = agregarProductos(listaProductos, '-Magdalenas blueberry:300$ c/u');
-                    console.log(elTotal);
-                    alert(listaProductos)
-                    break;
-
-                case "5":
-                    elTotal = suma(elTotal, precioTortaSweetDream);
-                    listaProductos = agregarProductos(listaProductos, '-Torta sweet dream:6950$');
-                    console.log(elTotal);
-                    alert(listaProductos)
-
-                case "6":
-                    elTotal = suma(elTotal, precioMagdalenasMouse );
-                    listaProductos = agregarProductos(listaProductos, '-Magdalenas mouse:360$ c/u');
-                    console.log(elTotal);
-                    alert(listaProductos)
-                case "7":
-                    elTotal = suma(elTotal, precioTortaTeGree);
-                    listaProductos = agregarProductos(listaProductos, '-Torta te gree:6900$');
-                    console.log(elTotal);
-                    alert(listaProductos)
-
-                case "8":    
-                    elTotal = suma(elTotal, precioMagdalenasFlorales);
-                    listaProductos = agregarProductos(listaProductos, '-Magdalenas florales:300$ c/u');
-                    console.log(elTotal);
-                    alert(listaProductos)
-
-                case "9":
-                    irAlProducto();
-                    break;
-
-                case "10":
-                    finalizarCompra(elTotal, listaProductos);
-                    compra = false;
-                    break;
-                default:
-                    alert("La opcion ingresada es incorrecta.");
-            }
+                break;
+            case "2":
+                if (miCarrito[1] != undefined) {
+                    sacar(miCarrito[1], miCarrito);
+                }
+                break;
+            case "3":
+                if (miCarrito[2] != undefined) {
+                    sacar(miCarrito[2], miCarrito);
+                }
+                break;
+            case "4":
+                if (miCarrito[3] != undefined) {
+                    sacar(miCarrito[3], miCarrito);
+                }
+                break;
+            case "5":
+                if (miCarrito[4] != undefined) {
+                    sacar(miCarrito[4], miCarrito);
+                }
+                break;
+            case "6":
+                if (miCarrito[5] != undefined) {
+                    sacar(miCarrito[5], miCarrito);
+                }
+                break;
+            case "7":
+                if (miCarrito[6] != undefined) {
+                    sacar(miCarrito[6], miCarrito);
+                }
+            case "8":
+                if (miCarrito[7] != undefined) {
+                    sacar(miCarrito[7], miCarrito);
+                }
+                break;
+            case "9":
+                compra = false;
+                break;
+            default:
+                alert("La opcion ingresada fue invalida intentelo de nuevo.");
         }
-
     }
+}
+//Menu principal con opciones
+function home() {
+    const miCarrito = [];
+    const listaProductos = [];
+    getProductos(listaProductos);
+    alert("Bienvenido a AmazingBake\nLugar de la mejor pasteleria de Argentina!");
+    let comprando = true;
+    while (comprando == true) {
+        let op = prompt("Opciones\n1-Acceder al menu\n2-Eliminar productos del carrito\n3-Finalizar compra");
+        switch (op) {
+            case "1":
+                llenarCarrito(listaProductos, miCarrito);
+                break;
+            case "2":
+                vaciarCarrito(miCarrito)
+                break;
+            case "3":
+                alert(showCarrito(miCarrito));
+                comprando = false;
+                break;
+            default:
+                alert("La opcion ingresada fue invalida intentelo de nuevo.");
+        }
+    }
+}
+home();
 
-inicio()
+
